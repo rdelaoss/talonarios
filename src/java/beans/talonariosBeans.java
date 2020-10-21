4ab3edada2951acab700e778cdca9f8f63d6337d
Arrupe Development Team <rdelaoss@gmail.com>
2020-10-21 14:12:04 -0600

    2020-10-21_1411AM_TALONARIOS_LECTOR_JSF

--- src/java/beans/talonariosBeans.java
+++ src/java/beans/talonariosBeans.java
@@ -1,33 +1,33 @@
 package beans;
 
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
-import javax.imageio.ImageIO;
 import javax.inject.Named;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-// |...:: [ ------------------- ] ::... |
-import com.idautomation.linear.*;
-import com.idautomation.linear.encoder.*;
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import javax.imageio.ImageIO;
-import javax.swing.JLabel;
+import java.io.Writer;
 
 @Named(value = "talonariosBeans")
 @SessionScoped
 public class talonariosBeans implements Serializable {
 
+//    private String inputContent = "Regueton";
+    private String alumnoSolicitudNumeroL = "-";
+
     public talonariosBeans() {
     }
 
-    public void preba() {
+    public String getAlumnoSolicitudNumeroL() {
+        return alumnoSolicitudNumeroL;
+    }
+
+    public void setAlumnoSolicitudNumeroL(String alumnoSolicitudNumeroL) {
+        this.alumnoSolicitudNumeroL = alumnoSolicitudNumeroL;
+    }
+
+    public void creargs1() {
         try {
             ProcessBuilder pb = new ProcessBuilder("java", "-jar", "C:\\barcodegs1\\creadorGs1.jar");
             Process p = pb.start();
@@ -39,10 +39,21 @@ public class talonariosBeans implements Serializable {
             int status = p.waitFor();
             System.out.println("Exited with status: " + status);
 
+            writeStream();
         } catch (Exception e) {
             System.out.println("ERROR : " + e);
         }
         System.out.println("AQUI TOY");
+    }
+
+    public void writeStream() throws IOException {
+        Writer writer = new FileWriter("C:\\barcodegs1\\outputOne.txt");
+
+        try {
+            writer.write(alumnoSolicitudNumeroL);
+        } finally {
+            writer.close();
+        }
     }
 
 }
